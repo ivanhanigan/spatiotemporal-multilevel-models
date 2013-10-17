1be1bedc9fe6528f533d2bd500c5c53c5e2eb0ad
ivanhanigan <ivanhanigan@gmail.com>
2013-10-18 10:06:21 +1100

    add comments to datachecking

--- NMMAPS-example/case-study-1-data-checking.r
+++ NMMAPS-example/case-study-1-data-checking.r
@@ -14,19 +14,28 @@ analyte$yy  <- substr(analyte$date, 1, 4)
 # do
 ## first we want to see if the age specific rates vary across the
 ## study sites
-stdysites  <- ddply(analyte, c("city", "agecat", "yy"),
-                     function(df) return(
-                       c(observed = sum(df[,"cvd"]),
-                         pop = mean(df[,"pop"]),
-                         crude.rate = sum(df[,"cvd"])/mean(df[,"pop"])
-                         )
-                       )
-                    )
-subset(stdysites, yy == 1987 & city == "batr")
-stdy <- subset(stdysites, yy == 2000)
+disease_colname <- "cvd"
+pop_colname <- "pop"
+by_cols  <- c("city", "agecat", "yy")
 
-## standard
-standard  <- ddply(stdysites, c("agecat", "yy"),
+stdysites  <- ddply(analyte, by_cols,
+                    function(df) return(
+                      c(observed = sum(df[,disease_colname]),
+                        pop = mean(df[,pop_colname]),
+                        crude.rate = sum(df[,disease_colname])/mean(df[,pop_colname])
+                        )
+                      )
+                    )
+## check
+head(stdysites)
+subset(stdysites, yy == 1987 & city == "batr")
+
+## define the subset we will use just the deaths and pop in 2000
+stdy <- subset(stdysites, yy == 2000)
+stdy
+
+## now define the standard population as the entire country
+standard  <- ddply(stdy, c("agecat"),
                      function(df) return(
                        c(observed = sum(df[,"observed"]),
                          pop = sum(df[,"pop"]),
@@ -35,12 +44,13 @@ standard  <- ddply(stdysites, c("agecat", "yy"),
                          )
                        )
                     )
-stnd <- subset(standard, yy == 2000)
+standard
 
-stdyByStnd  <- merge(stdy, stnd, by = "agecat")
+## Merge the studysites and the standard
+stdyByStnd  <- merge(stdy, standard, by = "agecat")
 stdyByStnd
 
-## ratio
+## plot the rate ratios
 png("images/ratio-stdy-by-stnd.png")
 mp <- barplot(stdyByStnd$crude.rate.x/stdyByStnd$crude.rate.y)
 text(mp, par("usr")[3],
@@ -50,13 +60,17 @@ text(mp, par("usr")[3],
 abline(1,0)
 dev.off()
 
-## Second we will check if populations vary
+## we can see that LA has an issue with the 65to74 agecat
+
+## Second we will check if ratio of the proportions in each population
+## agecat vary between study sites and the standard
 totals <- ddply(stdyByStnd, c("city"), summarise,
                 sum(pop.x)
                 )
 totals <- merge(stdyByStnd[,c("city","agecat","pop.x")], totals)
 totals$pop.wt  <- totals[,3] / totals[,4]
 totals <- arrange(totals, city, agecat)
+totals
 
 totalsStnd <- ddply(stdyByStnd, c("agecat"), summarise,
                 sum(pop.x)
@@ -64,9 +78,12 @@ totalsStnd <- ddply(stdyByStnd, c("agecat"), summarise,
 totalsStnd$totalPop <- sum(totalsStnd[,2])
 totalsStnd$pop.wt.total <- totalsStnd[,2]/totalsStnd[,3]
 totalsStnd
-totals
+
+## merge these so we can look at the ratios
 totals2 <- merge(totals, totalsStnd, by = "agecat")
 totals2 <- arrange(totals2, agecat, city)
+
+## now plot the ratios
 png("images/ratio-stdy-by-stnd-pops.png")
 mp <- barplot(totals2$pop.wt/totals2$pop.wt.total)
 text(mp, par("usr")[3],
@@ -75,3 +92,6 @@ text(mp, par("usr")[3],
      )
 abline(1,0)
 dev.off()
+
+## and we can see that there are far fewer 65to74 aged persons in LA
+## than expected.
